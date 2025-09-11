a634e8979e4681c1565ab628b36f1a1bf8accc05
Brad Farleigh <brad@bangonline.com.au>
2025-09-11 14:31:55 +0800

    feat: update SEO titles and add JSON-LD schema markup for village pages
    - Update SEO title format to 'Retirement Villages - %village name% | Amana Living'
    - Add comprehensive JSON-LD schema markup for both Moline Village and Collier Park Village
    - Include Place, NGO, and BreadcrumbList schema types
    - Add OpenGraph metadata for social media sharing
    - Enhance search engine visibility and rich snippets display

--- src/app/(landing-pages)/collier-park/page.tsx
+++ src/app/(landing-pages)/collier-park/page.tsx
@@ -1,6 +1,104 @@
+import { Metadata } from 'next'
 import { VillageTemplate } from '@/components/templates/VillageTemplate'
 import { collierParkData } from '@/data/villages/collier-park'
 
+export const metadata: Metadata = {
+  title: 'Retirement Villages - Collier Park Village | Amana Living',
+  description: 'Nestled in Como, Collier Park Village provides exclusive independent living accommodation for individuals aged 55 and above, surrounded by beautifully landscaped gardens.',
+  openGraph: {
+    title: 'Retirement Villages - Collier Park Village | Amana Living',
+    description: 'Nestled in Como, Collier Park Village provides exclusive independent living accommodation for individuals aged 55 and above, surrounded by beautifully landscaped gardens.',
+    images: ['https://s3.ap-southeast-2.amazonaws.com/amana-living/assets/retirementVillageLocations/photos/collier-park/_1200x630_crop_center-center_82_none/Collier-Park-Village1-Como-EX-9.jpg?mtime=1719898186'],
+    url: 'https://www.amanaliving.com.au/retirement-villages/locations/collier-park',
+  },
+}
+
 export default function CollierParkPage() {
-  return <VillageTemplate villageData={collierParkData} />
+  const schema = {
+    "@context": "http://schema.org",
+    "@graph": [
+      {
+        "@type": "Place",
+        "description": "Nestled in Como, Collier Park Village provides exclusive independent living accommodation for individuals aged 55 and above, surrounded by beautifully landscaped gardens. Our village boasts 169 two-bedroom independent villas, along with community and leisure centres, all designed to enhance your living experience. We are also pet friendly!  Collier Park Village offers two meeting venues on-site for our residents' convenience. The Community Centre, available to residents and their families, is a charming and well-equipped space for social gatherings and community events. Our village thrives on a vibrant social committee and the generous volunteerism of residents, ensuring a diverse range of activities and events such as cards, bingo, afternoon teas, and exercise classes, all held exclusively for our residents' enjoyment in the Community Centre. Additionally, our comprehensive library offers a wide selection of movies for borrowing, providing ample opportunities for relaxation and entertainment.",
+        "image": {
+          "@type": "ImageObject",
+          "url": "https://s3.ap-southeast-2.amazonaws.com/amana-living/assets/retirementVillageLocations/photos/collier-park/_1200x630_crop_center-center_82_none/Collier-Park-Village1-Como-EX-9.jpg?mtime=1719898186"
+        },
+        "mainEntityOfPage": "https://www.amanaliving.com.au/retirement-villages/locations/collier-park",
+        "name": "Retirement Villages  - Collier Park Village",
+        "url": "https://www.amanaliving.com.au/retirement-villages/locations/collier-park"
+      },
+      {
+        "@id": "https://www.amanaliving.com.au/#identity",
+        "@type": "NGO",
+        "description": "Amana Living is one of WA's largest not-for-profit providers of care and services for older people.",
+        "image": {
+          "@type": "ImageObject",
+          "height": "108",
+          "url": "https://amana-living.s3.ap-southeast-2.amazonaws.com/assets/icons/logo.svg?mtime=20210729112657&focal=none",
+          "width": "142"
+        },
+        "logo": {
+          "@type": "ImageObject",
+          "height": "60",
+          "url": "https://s3.ap-southeast-2.amazonaws.com/amana-living/assets/icons/_600x60_fit_center-center_82_none/102086/logo.jpg?mtime=1627529217",
+          "width": "79"
+        },
+        "name": "Amana Living Aged Care Services",
+        "sameAs": [
+          "https://www.twitter.com/amanaliving",
+          "facebook.com/amanaliving",
+          "https://www.linkedin.com/company/amana-living",
+          "https://www.youtube.com/user/Amanaliving",
+          "https://www.instagram.com/amana.living"
+        ],
+        "url": "https://www.amanaliving.com.au/"
+      },
+      {
+        "@id": "#creator",
+        "@type": "Organization"
+      },
+      {
+        "@type": "BreadcrumbList",
+        "description": "Breadcrumbs list",
+        "itemListElement": [
+          {
+            "@type": "ListItem",
+            "item": "https://www.amanaliving.com.au/",
+            "name": "Homepage",
+            "position": 1
+          },
+          {
+            "@type": "ListItem",
+            "item": "https://www.amanaliving.com.au/retirement-villages",
+            "name": "Retirement Villages",
+            "position": 2
+          },
+          {
+            "@type": "ListItem",
+            "item": "https://www.amanaliving.com.au/retirement-villages/locations",
+            "name": "Retirement Villages Locations",
+            "position": 3
+          },
+          {
+            "@type": "ListItem",
+            "item": "https://www.amanaliving.com.au/retirement-villages/locations/collier-park",
+            "name": "Collier Park Village",
+            "position": 4
+          }
+        ],
+        "name": "Breadcrumbs"
+      }
+    ]
+  }
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      />
+      <VillageTemplate villageData={collierParkData} />
+    </>
+  )
 }
--- src/app/(landing-pages)/moline-village/page.tsx
+++ src/app/(landing-pages)/moline-village/page.tsx
@@ -1,6 +1,104 @@
+import { Metadata } from 'next'
 import { VillageTemplate } from '@/components/templates/VillageTemplate'
 import { molineVillageData } from '@/data/villages/moline-village'
 
+export const metadata: Metadata = {
+  title: 'Retirement Villages - Moline Village | Amana Living',
+  description: 'Moline Village is the perfect spot to take advantage of the enviable Karrinyup lifestyle, close to Trigg Beach, and the multi-million dollar redevelopments of Scarborough Beach and Karrinyup Shopping Centre.',
+  openGraph: {
+    title: 'Retirement Villages - Moline Village | Amana Living',
+    description: 'Moline Village is the perfect spot to take advantage of the enviable Karrinyup lifestyle, close to Trigg Beach, and the multi-million dollar redevelopments of Scarborough Beach and Karrinyup Shopping Centre.',
+    images: ['https://s3.ap-southeast-2.amazonaws.com/amana-living/assets/retirementVillageLocations/photos/moline-village/_1200x630_crop_center-center_82_none/0550.jpg?mtime=1696391375'],
+    url: 'https://www.amanaliving.com.au/retirement-villages/locations/moline-village',
+  },
+}
+
 export default function MolineVillagePage() {
-  return <VillageTemplate villageData={molineVillageData} />
+  const schema = {
+    "@context": "http://schema.org",
+    "@graph": [
+      {
+        "@type": "Place",
+        "description": "Moline Village is the perfect spot to take advantage of the enviable Karrinyup lifestyle, close to Trigg Beach, and the multi-million dollar redevelopments of Scarborough Beach and Karrinyup Shopping Centre. Enjoy scenic walks in the Trigg Bushland Reserve, hit a hole in one at a local golf course or head to Perth's CBD which is only 14km away.   Designed for over 55s, the village offers easy living with low maintenance homes in a tranquil setting. Feel safe and secure in this friendly village which has an active and welcoming community. You can take part in the many social activities or make the most of the village's amenities including swimming pool, mini gym, library, bocce court, mini golf range and more.  Each floor has a large communal sunroom where you can unwind with a beverage and a good book or meet up with friends. Plus, there is a general store run by resident volunteers, and visiting services including hair salon, beauty therapist, massage therapist, and podiatrist.  There's also a weekly shopper bus service to the local shopping centre.  These lease for life apartments are available to view by appointment.",
+        "image": {
+          "@type": "ImageObject",
+          "url": "https://s3.ap-southeast-2.amazonaws.com/amana-living/assets/retirementVillageLocations/photos/moline-village/_1200x630_crop_center-center_82_none/0550.jpg?mtime=1696391375"
+        },
+        "mainEntityOfPage": "https://www.amanaliving.com.au/retirement-villages/locations/moline-village",
+        "name": "Retirement Villages  - Moline Village",
+        "url": "https://www.amanaliving.com.au/retirement-villages/locations/moline-village"
+      },
+      {
+        "@id": "https://www.amanaliving.com.au/#identity",
+        "@type": "NGO",
+        "description": "Amana Living is one of WA's largest not-for-profit providers of care and services for older people.",
+        "image": {
+          "@type": "ImageObject",
+          "height": "108",
+          "url": "https://amana-living.s3.ap-southeast-2.amazonaws.com/assets/icons/logo.svg?mtime=20210729112657&focal=none",
+          "width": "142"
+        },
+        "logo": {
+          "@type": "ImageObject",
+          "height": "60",
+          "url": "https://s3.ap-southeast-2.amazonaws.com/amana-living/assets/icons/_600x60_fit_center-center_82_none/102086/logo.jpg?mtime=1627529217",
+          "width": "79"
+        },
+        "name": "Amana Living Aged Care Services",
+        "sameAs": [
+          "https://www.twitter.com/amanaliving",
+          "facebook.com/amanaliving",
+          "https://www.linkedin.com/company/amana-living",
+          "https://www.youtube.com/user/Amanaliving",
+          "https://www.instagram.com/amana.living"
+        ],
+        "url": "https://www.amanaliving.com.au/"
+      },
+      {
+        "@id": "#creator",
+        "@type": "Organization"
+      },
+      {
+        "@type": "BreadcrumbList",
+        "description": "Breadcrumbs list",
+        "itemListElement": [
+          {
+            "@type": "ListItem",
+            "item": "https://www.amanaliving.com.au/",
+            "name": "Homepage",
+            "position": 1
+          },
+          {
+            "@type": "ListItem",
+            "item": "https://www.amanaliving.com.au/retirement-villages",
+            "name": "Retirement Villages",
+            "position": 2
+          },
+          {
+            "@type": "ListItem",
+            "item": "https://www.amanaliving.com.au/retirement-villages/locations",
+            "name": "Retirement Villages Locations",
+            "position": 3
+          },
+          {
+            "@type": "ListItem",
+            "item": "https://www.amanaliving.com.au/retirement-villages/locations/moline-village",
+            "name": "Moline Village",
+            "position": 4
+          }
+        ],
+        "name": "Breadcrumbs"
+      }
+    ]
+  }
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      />
+      <VillageTemplate villageData={molineVillageData} />
+    </>
+  )
 }
